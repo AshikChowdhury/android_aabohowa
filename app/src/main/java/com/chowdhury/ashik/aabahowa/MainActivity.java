6e2945373edfd73c68a1f39babbb5b07ad22edef
Ashik Chowdhury <ashikbracu@gmail.com>
2019-09-04 00:29:20 +0600

    Location find and gecode set done

--- app/src/main/java/com/chowdhury/ashik/aabahowa/MainActivity.java
+++ app/src/main/java/com/chowdhury/ashik/aabahowa/MainActivity.java
@@ -6,20 +6,41 @@ import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
     private FusedLocationProviderClient client;
-
+    private TextView latlonTV;
+    private Geocoder geocoder;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        latlonTV = findViewById(R.id.latlonTV);
+        geocoder = new Geocoder(this, Locale.getDefault());
         client = LocationServices.getFusedLocationProviderClient(this);
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (checkLocationPermission()){
+            getDeviceLastLocation();
+        }
     }
 
     private boolean checkLocationPermission(){
@@ -34,6 +55,42 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-//        if (requestCode == 111 && permissions)
+        if (requestCode == 111 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            getDeviceLastLocation();
+        }else{
+            //explaion why this permission is important
+        }
+    }
+    private void convertGeolocationToStreet(double lat, double lon) throws IOException{
+        List<Address> addressList = geocoder.getFromLocation(lat, lon, 1);
+        final Address address = addressList.get(0);
+        String addressLine = address.getAddressLine(0);
+        latlonTV.setText(lat+","+lon+"\n"+addressLine);
+    }
+
+    private void getDeviceLastLocation(){
+        if (checkLocationPermission()){
+            client.getLastLocation()
+            .addOnSuccessListener(new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+                    if (location != null){
+                        double lat = location.getLatitude();
+                        double lon = location.getLongitude();
+                        try {
+                            convertGeolocationToStreet(lat, lon);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+//                        latlonTV.setText(lat+","+lon);
+                    }
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Log.e("failed", e.getLocalizedMessage() );
+                }
+            });
+        }
     }
 }
