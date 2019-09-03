6e26790b4c430f3d41b678f126711121167a0628
AshikChowdhury <ashikbracu@gmail.com>
2019-09-03 18:09:30 +0600

    add permission ask

--- app/src/main/java/com/chowdhury/ashik/aabahowa/MainActivity.java
+++ app/src/main/java/com/chowdhury/ashik/aabahowa/MainActivity.java
@@ -1,14 +1,39 @@
 package com.chowdhury.ashik.aabahowa;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+
 public class MainActivity extends AppCompatActivity {
+    private FusedLocationProviderClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        client = LocationServices.getFusedLocationProviderClient(this);
+
+    }
+
+    private boolean checkLocationPermission(){
+        String[] permissions = {Manifest.permission.ACCESS_FINE_LOCATION};
+       if (ActivityCompat.checkSelfPermission(this,
+               Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+           ActivityCompat.requestPermissions(this, permissions, 111);
+           return false;
+       }
+       return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+//        if (requestCode == 111 && permissions)
     }
 }
